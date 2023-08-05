cc95f145d3a6561f3e7e8678872e2988caece68d
Alaska-999 <kristinagaranchuk@gmail.com>
2023-08-05 23:08:48 +0300

    responsive slider

--- src/components/Trips/TripsSlider.tsx
+++ src/components/Trips/TripsSlider.tsx
@@ -4,8 +4,18 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import {ITrip} from '../../types/types.ts';
 import Trip from './Trip';
-import './Trips.css'
+import './TripsSlider.css';
 import {FC} from "react";
+
+interface ITripsSlider {
+    trips: ITrip[];
+    handleTripActive: (city: string, startDate: string, endDate: string) => void;
+    activeTrip: {
+        city: string;
+        startDate: string;
+        endDate: string;
+    } | null;
+}
 
 interface ITripsSlider {
     trips: ITrip[];
@@ -17,38 +27,48 @@ interface ITripsSlider {
     } | null
 }
 
-const TripSlider: FC<ITripsSlider>= ({
-                        trips,
-                        handleTripActive,
-                        activeTrip
-                    }) => {
-    const sliderWidth = 300 * Math.min(trips.length, 3);
+const TripSlider: FC<ITripsSlider> = ({
+                                          trips,
+                                          handleTripActive,
+                                          activeTrip
+
+                                      }) => {
+
+
+
+    const sliderWidth = window.innerWidth <= 1800 ? 310 * Math.min(trips.length, 2) : 300 * Math.min(trips.length, 3);
+    const slidesView = window.innerWidth <= 1800 ? Math.min(trips.length, 2)  : Math.min(trips.length, 3);
 
     return (
         <Swiper
             spaceBetween={60}
             modules={[Navigation]}
-            slidesPerView={Math.min(trips.length, 3)}
+            slidesPerView={slidesView}
             className="swiper"
             navigation
-            style={{width: `${sliderWidth}px`}}
+            style={{width: `${sliderWidth}px`
+        }}
         >
-            {trips.map((trip) => (
-                <SwiperSlide key={Math.random()}>
-                    <Trip
-                        city={trip.city}
-                        startDate={trip.startDate}
-                        endDate={trip.endDate}
-                        image={trip.image}
-                        isActive={
-                            trip.city === activeTrip?.city &&
-                            trip.startDate === activeTrip?.startDate &&
-                            trip.endDate === activeTrip?.endDate
-                        }
-                        onClick={() => handleTripActive(trip.city, trip.startDate, trip.endDate)}
-                    />
-                </SwiperSlide>
-            ))}
+            {
+                trips.map((trip) => (
+                    <div>
+                        <SwiperSlide key={Math.random()}>
+                            <Trip
+                                city={trip.city}
+                                startDate={trip.startDate}
+                                endDate={trip.endDate}
+                                image={trip.image}
+                                isActive={
+                                    trip.city === activeTrip?.city &&
+                                    trip.startDate === activeTrip?.startDate &&
+                                    trip.endDate === activeTrip?.endDate
+                                }
+                                onClick={() => handleTripActive(trip.city, trip.startDate, trip.endDate)}
+                            />
+                        </SwiperSlide>
+                    </div>
+                ))
+            }
         </Swiper>
     );
 };
